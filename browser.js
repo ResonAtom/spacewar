5af5b9cc9abd3029d8aa848ad61fbd4c1a118608
Adam <adam@objectivegames.com>
2020-08-24 17:56:35 -0500

    Generate certs with node-forge, parse link lines better, handle link clicks, 'back' nagivation

--- browser.js
+++ browser.js
@@ -1,5 +1,25 @@
 let ipcRenderer = require('electron').ipcRenderer;
-ipcRenderer.on('response', function (event, data) {
-    console.log(data);
-    document.getElementById('content').innerHTML += data.join('<br/>')
-});
+ipcRenderer.on('response', function (event, [host, lines]) {
+    console.log(lines);
+    window.scrollTo(0, 0)
+    lines = lines.map(line => {
+        if(line.slice(0, 2) === '=>') { // Link
+            const linkContent = line.slice(2)
+
+            // Hmm.  First find the first text.  
+            const indexFirstText = linkContent.search(/\S+/)
+            // Then find the first whitespace after that point.
+            const indexNextWhitespace = linkContent.slice(indexFirstText).search(/\s+/, indexFirstText)
+
+            const url = linkContent.slice(indexFirstText, indexNextWhitespace + 1).trim()
+            const label = linkContent.slice(indexNextWhitespace + 1).trim()
+
+            const hasScheme = url.indexOf('://') !== -1
+            const fullUrl = hasScheme ? url : `gemini://${host}/${url}`
+
+            return `<a href="${fullUrl}">${label}</a>`
+        }
+        return line
+    })
+    document.getElementById('content').innerHTML = lines.join('<br/>')
+})
--- main.js
+++ main.js
@@ -1,9 +1,16 @@
 const { app, BrowserWindow } = require('electron')
+const { connect } = require('tls')
+const { readFileSync } = require('fs')
 
-let win; // todo
-app.whenReady().then(() => {
+let win = undefined
+let history = []
+let hindex = 0
+
+const [key, cert] = createCustomCert()
+
+async function createWindow() {
     win = new BrowserWindow({
-        width: 800,
+        width: 1400,
         height: 600,
         backgroundColor: '#fff',
         webPreferences: {
@@ -14,61 +21,181 @@ app.whenReady().then(() => {
 
     win.loadFile('index.html')
     win.webContents.openDevTools()
-})
+
+    const goBack = () => {
+        if(hindex > 0) {
+            hindex -= 1
+            loadPage(history[hindex], true)
+        }
+    }
+
+    win.webContents.on('will-navigate', (e, gurl) => {
+        console.log('Click intercept: '+gurl)
+        e.preventDefault()
+        if(gurl === 'gemini://back') {
+            goBack()
+        } else {
+            loadPage(gurl) // todo :)
+        }
+    })
+
+    win.webContents.on('before-input-event', (event, input) => {
+        // For example, only enable application menu keyboard shortcuts when Ctrl/Cmd are down.
+        // win.webContents.setIgnoreMenuShortcuts(!input.control && !input.meta)
+        // console.log('before-input-event', event, input)
+        if(input.type === 'keyDown' && input.key === 'b') {
+            goBack()
+        }
+    })
+    win.webContents.on('app-command', (event, cmd) => {
+        console.log("app-command", event, cmd)
+        if (cmd === 'browser-backward' && win.webContents.canGoBack()) {
+            win.webContents.goBack()
+        }
+    })
+
+    // const url = 'gemini://gemini.circumlunar.space' // TODO handle this redirect
+    const home = 'gemini://gemini.circumlunar.space/'
+    loadPage(home)
+}
+
+async function loadPage(gurl, inHistory = false) {
+    if(!inHistory) {
+        history = history.slice(0, hindex +1) // Wipe out history after this point
+        history.push(gurl)
+        hindex = history.length -1
+    }
+
+    const path = gurl.slice(gurl.indexOf('://') + '://'.length)
+    const indexEndSlash = path.indexOf('/') 
+    const hasEndSlash = indexEndSlash !== -1
+    const host = hasEndSlash ? path.slice(0, indexEndSlash) : path
+    console.log("host", host)
+
+    let conn = connect(1965, host, {key: key, cert: cert}, function() {
+        console.log(`Connection ${conn.authorized ? 'authorized' : 'not authorized: ' + conn.authorizationError}`)
+        conn.write(`${gurl}\r\n`)
+    })
+
+    let lines = [];
+    conn.on('data', function (data) {
+        const parts = data.toString().split('\r\n')
+        console.log(`Parts: ${parts.length}`)
+
+        if(parts.length === 2) {
+            const header = parts[0]
+            console.log(`Header: ${header}`)
+            parts.shift() // remove header line, body is now in place [0]
+        }
+        lines = [...lines, ...parts[0].split('\n')]
+    })
+
+    conn.on('end', () => {
+        console.log('server ends connection')
+        win.webContents.send('response', [host, lines])
+        // conn.end() // Server should handle ending connection
+    })
+}
 
 app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') { // on macOS don't exit when last window is closed
         app.quit()
     }
 })
-
 app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) { // on macOS create a new window if app icon clicked
         createWindow()
     }
 })
 
+app.whenReady().then(createWindow)
 
-// 1.1 Gemini transactions
-// C: Opens connection S: Accepts connection C/S: Complete TLS handshake (see section 4) C: Validates server certificate (see 4.2) C: Sends request (one CRLF terminated line) (see section 2) S: Sends response header (one CRLF terminated line), closes connection under non-success conditions (see 3.1 and 3.2) S: Sends response body (text or binary data) (see 3.3) S: Closes connection C: Handles response (see 3.4)
-// Gemini requests are a single CRLF-terminated line with the following structure:
-// <URL><CR><LF></LF>
+function createCustomCert() { // Custom cert time!
+    const forge = require('node-forge');
+    var pki = forge.pki;
 
-// gemini://gemini.circumlunar.space
+    // generate a keypair and create an X.509v3 certificate
+    var keys = pki.rsa.generateKeyPair(2048);
+    var cert = pki.createCertificate();
+    cert.publicKey = keys.publicKey;
+    // alternatively set public key from a csr
+    //cert.publicKey = csr.publicKey;
+    // NOTE: serialNumber is the hex encoded value of an ASN.1 INTEGER.
+    // Conforming CAs should ensure serialNumber is:
+    // - no more than 20 octets
+    // - non-negative (prefix a '00' if your value starts with a '1' bit)
+    cert.serialNumber = '01';
+    cert.validity.notBefore = new Date();
+    cert.validity.notAfter = new Date();
+    cert.validity.notAfter.setFullYear(cert.validity.notBefore.getFullYear() + 1);
+    var attrs = [{
+        name: 'commonName',
+        value: 'example.org'
+    }, {
+        name: 'countryName',
+        value: 'US'
+    }, {
+        shortName: 'ST',
+        value: 'Virginia'
+    }, {
+        name: 'localityName',
+        value: 'Blacksburg'
+    }, {
+        name: 'organizationName',
+        value: 'Test'
+    }, {
+        shortName: 'OU',
+        value: 'Test'
+    }];
+    cert.setSubject(attrs);
+    // alternatively set subject from a csr
+    //cert.setSubject(csr.subject.attributes);
+    cert.setIssuer(attrs);
+    cert.setExtensions([{
+    name: 'basicConstraints',
+    cA: true
+    }, {
+    name: 'keyUsage',
+    keyCertSign: true,
+    digitalSignature: true,
+    nonRepudiation: true,
+    keyEncipherment: true,
+    dataEncipherment: true
+    }, {
+    name: 'extKeyUsage',
+    serverAuth: true,
+    clientAuth: true,
+    codeSigning: true,
+    emailProtection: true,
+    timeStamping: true
+    }, {
+    name: 'nsCertType',
+    client: true,
+    server: true,
+    email: true,
+    objsign: true,
+    sslCA: true,
+    emailCA: true,
+    objCA: true
+    }, {
+    name: 'subjectAltName',
+    altNames: [{
+        type: 6, // URI
+        value: 'http://example.org/webid#me'
+    }, {
+        type: 7, // IP
+        ip: '127.0.0.1'
+    }]
+    }, {
+    name: 'subjectKeyIdentifier'
+    }]);
 
-const { connect } = require('tls')
-const { readFileSync } = require('fs')
+    // self-sign certificate
+    // signs a certificate using SHA-256 instead of SHA-1
+    cert.sign(keys.privateKey, forge.md.sha256.create());
 
-const options = {
-    key: readFileSync('certs/private-key.pem'),
-    cert: readFileSync('certs/public-cert.pem')
+    var publicCertPem = pki.certificateToPem(cert);
+    var privateKeyPem = pki.privateKeyToPem(keys.privateKey);
+
+    return [privateKeyPem, publicCertPem]
 }
-
-// const url = 'gemini://gemini.circumlunar.space' // will respond with redirect
-const url = 'gemini://gemini.circumlunar.space/'
-let conn = connect(1965, 'gemini.circumlunar.space', options, function() {
-    if (conn.authorized) {
-        console.log("Connection authorized by a Certificate Authority.")
-    } else {
-        console.log("Connection not authorized: " + conn.authorizationError)
-    }
-    console.log()
-
-    conn.write(`${url}\r\n`)
-})
-
-conn.on('data', function (data) {
-    const lines = data.toString().split('\n')
-    console.log(lines.length);
-
-    if(lines.length === 2) {
-        console.log(`Header: ${lines[0]}`)
-    } else {
-        lines.shift() // remove first line
-        win.webContents.send('response', lines)
-    }
-
-    conn.end()
-
-
-})
